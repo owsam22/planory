4a85dacaa24a9b0f555dd8d026ddb0b2e0210f5f
sam <22.samarpan@gmail.com>
2026-05-11 16:50:26 +0530

    google login added:schema redesigned for login

--- server/models.js
+++ server/models.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    googleId: { type: String, unique: true, sparse: true },
+    email: { type: String, unique: true, sparse: true },
+    username: { type: String, required: true },
+    password: { type: String },
+    avatar: { type: String },
     timezone: { type: String, default: 'Asia/Kolkata' },
     nightMode: { type: Boolean, default: false },
     workHours: {
